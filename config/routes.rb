744c4890ca02eac0b7e4e7c11e9c584bc1c9b22d
Kyle Cho <kylecho.work@gmail.com>
2016-09-20 16:11:19 -0700

    adds the product create, update, and destroy action nested on the user resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   # Api definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1,
-              constraints: ApiConstraints.new(version: 1, default: true) do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, :only => [:show, :create, :update, :destroy]
+      resources :users, :only => [:show, :create, :update, :destroy] do
+        # exposing user id for their products
+        resources :products, :only => [:create, :update, :destroy]
+      end
       resources :sessions, :only => [:create, :destroy]
       resources :products, :only => [:show, :index]
     end
